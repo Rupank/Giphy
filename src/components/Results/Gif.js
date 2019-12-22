58246d1f53105a9d3b979e98fe1ec132350dabd1
Rupank Bansal <rupank@playpowerlabs.com>
2019-12-22 20:23:13 +0530

    Removed Unnecessary Code

--- src/components/Results/Gif.js
+++ src/components/Results/Gif.js
@@ -2,28 +2,20 @@ import React, { useState } from 'react'
 import "../../App.css";
 
 
-function getOrientation(width, height) {
-    if (width > height) {
-        return 'landscape';
-    }
-    else {
-        return 'portrait';
-    }
-}
 function Gif(props) {
     const [gifState, setGifState] = useState(true);
 
     const toggleGifState = () => {
         setGifState(!gifState);
     }
+
     let gifURL = props.data.images.fixed_height_downsampled.url;
     if (!gifState) {
         gifURL = props.data.images.fixed_height_still.url;
     }
     return (
         <>
-            <img className={getOrientation(props.data.images.fixed_height_downsampled.width, props.data.images.fixed_height_downsampled.height)}
-                src={gifURL} alt={props.data.title} onClick={toggleGifState} />
+            <img src={gifURL} alt={props.data.title} onClick={toggleGifState} />
         </>
     )
 }
