028bac58282ca88a206d1c702b77dc5b5aa102d6
Rupank Bansal <rupank@playpowerlabs.com>
2019-12-23 15:37:24 +0530

    Added Basic Testing Setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from './store/createStore';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders App Component', () => {
+
+  const { asFragment } = render(<Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>);
+  expect(asFragment()).toMatchSnapshot();
 });
